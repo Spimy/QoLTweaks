361fd72060f4947d61358047c5a57feb72bb6c68
Spimy <officialspyme@gmail.com>
2022-12-18 00:44:11 +0400

    Add nametag shear permission node

--- src/main/java/dev/spimy/qoltweaks/Permissions.java
+++ src/main/java/dev/spimy/qoltweaks/Permissions.java
@@ -2,7 +2,8 @@ package dev.spimy.qoltweaks;
 
 public enum Permissions {
     LADDERWARP("ladderwarp"),
-    BOOKEXTRACT("bookextract");
+    BOOK_EXTRACT("bookextract"),
+    NAMETAG_SHEAR("nametagshear");
 
     private final String permissionNode;
 
