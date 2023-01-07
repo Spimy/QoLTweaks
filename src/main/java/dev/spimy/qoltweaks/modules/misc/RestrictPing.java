9a738750a8dd65f2cc4122203f20bf1460203a9f
Spimy <officialspyme@gmail.com>
2023-01-07 21:50:30 +0400

    Add ping cancellation from mcstatus

--- src/main/java/dev/spimy/qoltweaks/modules/misc/RestrictPing.java
+++ src/main/java/dev/spimy/qoltweaks/modules/misc/RestrictPing.java
@@ -1,5 +1,11 @@
 package dev.spimy.qoltweaks.modules.misc;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
 import dev.spimy.qoltweaks.Modules;
 import dev.spimy.qoltweaks.QoLTweaks;
@@ -10,6 +16,7 @@ import org.bukkit.event.Listener;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.net.InetSocketAddress;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Objects;
@@ -25,27 +32,32 @@ public class RestrictPing implements Listener {
 
     @EventHandler()
     public void onPing(PaperServerListPingEvent event) {
-        try {
-            ConfigurationSection config = plugin.configManager.getModuleConfig(Modules.MISC);
-            if (!config.getBoolean("restrict-ping.enabled")) return;
 
-            String hostName = Objects.requireNonNull(event.getClient().getVirtualHost()).getHostName();
-            Set<String> whitelistedHostnames = new HashSet<>(plugin.getConfig().getStringList("allowed-hostnames"));
+        ConfigurationSection config = plugin.configManager.getModuleConfig(Modules.MISC);
+        if (!config.getBoolean("restrict-ping.enabled")) return;
 
-            if (whitelistedHostnames.contains(hostName)) return;
+        InetSocketAddress virtualAddress = event.getClient().getVirtualHost();
+        if (virtualAddress == null) {
+            plugin.getLogger().warning("Someone tried to ping the server using an unknown address.");
             event.setCancelled(true);
-
-            final String ICON_URL = "https://media.minecraftforum.net/attachments/300/619/636977108000120237.png";
-            try {
-                URL url = new URL(ICON_URL);
-                BufferedImage img = ImageIO.read(url);
-                event.setServerIcon(Bukkit.loadServerIcon(img));
-            } catch (Exception e) {
-                return;
-            }
-        } catch (NullPointerException ex) {
             return;
         }
+
+        String hostName = virtualAddress.getHostName();
+        Set<String> whitelistedHostnames = new HashSet<>(plugin.getConfig().getStringList("allowed-hostnames"));
+
+        if (whitelistedHostnames.contains(hostName)) return;
+        event.setCancelled(true);
+
+        final String ICON_URL = "https://media.minecraftforum.net/attachments/300/619/636977108000120237.png";
+        try {
+            URL url = new URL(ICON_URL);
+            BufferedImage img = ImageIO.read(url);
+            event.setServerIcon(Bukkit.loadServerIcon(img));
+        } catch (Exception e) {
+            plugin.getLogger().warning("Failed to load offline server icon.");
+        }
+
     }
 
 }
