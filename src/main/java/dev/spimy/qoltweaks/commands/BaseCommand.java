2eb757505bc531b477f8f8b5804818c9dd0efebe
Spimy <officialspyme@gmail.com>
2023-01-14 00:10:04 +0400

    Add tab autocompletion

--- src/main/java/dev/spimy/qoltweaks/commands/BaseCommand.java
+++ src/main/java/dev/spimy/qoltweaks/commands/BaseCommand.java
@@ -4,42 +4,31 @@ import dev.spimy.qoltweaks.QoLTweaks;
 import dev.spimy.qoltweaks.commands.subcommands.SubCommand;
 import dev.spimy.qoltweaks.commands.subcommands.SubCommandHandler;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-public class BaseCommand implements CommandExecutor {
+public class BaseCommand implements TabExecutor {
 
-    private final QoLTweaks plugin;
-
-    public BaseCommand(QoLTweaks plugin) {
-        this.plugin = plugin;
-    }
+    private final QoLTweaks plugin = QoLTweaks.getInstance();
+    private final SubCommandHandler handler = new SubCommandHandler();
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        SubCommandHandler handler = new SubCommandHandler(plugin);
-
         if (args.length == 0) {
             return handler.getSubCommand("help").execute(sender, args);
         }
 
-        ConfigurationSection config = plugin.getConfig().getConfigurationSection("lang");
-
         SubCommand subCommand = handler.getSubCommand(args[0]);
         if (subCommand == null) {
-            sender.sendMessage(
-                plugin.formatMessage(
-                    String.format(
-                        "%s %s",
-                        config.getString("prefix"),
-                        config.getString("not-exist")
-                    )
-                )
-            );
+            sender.sendMessage(plugin.getMessageManager().getConfigMessage("not-exist", true));
             return true;
         }
 
@@ -49,4 +38,26 @@ public class BaseCommand implements CommandExecutor {
         );
     }
 
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        final List<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+            StringUtil.copyPartialMatches(args[0], handler.getSubCommandNames(), completions);
+        } else if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("help")) {
+                StringUtil.copyPartialMatches(args[1], handler.getSubCommandNames(), completions);
+            } else {
+                for (int i = 0; i < handler.getSubCommandNames().size(); i++) {
+                    if (args[0].equalsIgnoreCase(handler.getSubCommandNames().get(i))) {
+                        SubCommand subCommand = handler.getSubCommand(handler.getSubCommandNames().get(i));
+                        StringUtil.copyPartialMatches(args[1], subCommand.getStrippedArguments(), completions);
+                    }
+                }
+            }
+        }
+
+        Collections.sort(completions);
+        return completions;
+    }
 }
