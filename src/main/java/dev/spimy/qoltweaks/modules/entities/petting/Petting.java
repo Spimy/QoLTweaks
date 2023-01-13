4ad62afc1ae1b36cfef6b7ae4be1fe00f5afa4c0
Spimy <officialspyme@gmail.com>
2023-01-14 00:16:57 +0400

    Refactor to extend Module instead and add deaggro feature

--- src/main/java/dev/spimy/qoltweaks/modules/entities/petting/Petting.java
+++ src/main/java/dev/spimy/qoltweaks/modules/entities/petting/Petting.java
@@ -1,61 +1,63 @@
 package dev.spimy.qoltweaks.modules.entities.petting;
 
-import dev.spimy.qoltweaks.Modules;
-import dev.spimy.qoltweaks.Permissions;
 import dev.spimy.qoltweaks.QoLTweaks;
+import dev.spimy.qoltweaks.modules.Module;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
-public class Petting implements Listener {
+import java.util.HashMap;
 
-    private final QoLTweaks plugin;
+public class Petting extends Module {
 
-    public Petting(QoLTweaks plugin) {
-        this.plugin = plugin;
+    private final String PET_DOG_PERM_KEY = "dog";
+    private final String PET_CAT_PERM_KEY = "cat";
+    private final QoLTweaks plugin = QoLTweaks.getInstance();
+
+    public Petting() {
+        super(
+            new HashMap<>() {{
+                put("cooldown.dog", 60);
+                put("cooldown.cat", 60);
+                put("heal", true);
+                put("deaggro", true);
+            }}
+        );
+        addPermission(PET_DOG_PERM_KEY, "pet-dog");
+        addPermission(PET_CAT_PERM_KEY, "pet-cat");
     }
 
     @EventHandler
     public void onPetting(PlayerInteractEntityEvent event) {
         if (event.getHand() != EquipmentSlot.HAND) return;
-
-        ConfigurationSection config = plugin.configManager.getModuleConfig(Modules.ENTITIES);
-        if (!config.getBoolean("petting.enabled")) return;
+        if (isDisabled()) return;
 
         Player player = event.getPlayer();
         Entity entity = event.getRightClicked();
 
         if (player.getInventory().getItemInMainHand().getType() != Material.AIR || !player.isSneaking()) return;
-        if (entity.getType() != EntityType.WOLF && entity.getType() != EntityType.CAT) return;
 
-        long cooldownTimer = 20;
-        Sound petSound = Sound.ENTITY_EXPERIENCE_ORB_PICKUP;
+        long cooldownTimer = 60;
+        Sound petSound;
 
         if (entity.getType() == EntityType.WOLF) {
-            if (config.getBoolean(("petting.require-permission.pet-dog"))) {
-                if (!player.hasPermission(Permissions.PET_DOG.getPermissionNode())) return;
-            }
+            if (isMissingPermission(player, PET_DOG_PERM_KEY)) return;
             if (!((Wolf) entity).isSitting()) return;
-            cooldownTimer = config.getLong("petting.cooldown.dog", cooldownTimer);
+            cooldownTimer = getConfig().getLong("cooldown.dog", cooldownTimer);
             petSound = Sound.ENTITY_WOLF_WHINE;
-        }
-
-        if (entity.getType() == EntityType.CAT) {
-            if (config.getBoolean(("petting.require-permission.pet-cat"))) {
-                if (!player.hasPermission(Permissions.PET_CAT.getPermissionNode())) return;
-            }
+        } else if (entity.getType() == EntityType.CAT) {
+            if (isMissingPermission(player, PET_CAT_PERM_KEY)) return;
             if (!((Cat) entity).isSitting()) return;
-            cooldownTimer = config.getLong("petting.cooldown.cat", cooldownTimer);
+            cooldownTimer = getConfig().getLong("cooldown.cat", cooldownTimer);
             petSound = Sound.ENTITY_CAT_PURREOW;
-        }
+        } else return;
 
         Tameable pet = (Tameable) entity;
         PettingTimer pettingTimer = new PettingTimer(plugin, pet);
@@ -69,10 +71,28 @@ public class Petting implements Listener {
         player.swingMainHand();
         pettingTimer.setPetTime();
 
-        if (config.getBoolean("petting.heal") && pet.getHealth() < pet.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()) {
-            double health = pet.getHealth() + Math.random();
-            if (health < 20) {
-                pet.setHealth(health);
+
+        if (getConfig().getBoolean("heal")) {
+            final AttributeInstance petMaxHealth = pet.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+            if (petMaxHealth == null) {
+                plugin.getLogger().warning("Unable to heal pet using petting function.");
+                return;
+            }
+
+            if (pet.getHealth() < petMaxHealth.getValue()) {
+                double health = pet.getHealth() + Math.random();
+                if (health < 20) {
+                    pet.setHealth(health);
+                }
+            }
+        }
+
+        if (getConfig().getBoolean("deaggro")) {
+            Entity target = pet.getTarget();
+            if (target != null) {
+                if (target.getType() == EntityType.PLAYER) {
+                    pet.setTarget(null);
+                }
             }
         }
 
