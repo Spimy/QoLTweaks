cb68b6df93231faf2114a9be2a84fcb2863ff49e
Spimy <officialspyme@gmail.com>
2023-01-14 00:17:09 +0400

    Add anti enderman grief module

---
+++ src/main/java/dev/spimy/qoltweaks/modules/entities/AntiEndermanGrief.java
@@ -0,0 +1,26 @@
+package dev.spimy.qoltweaks.modules.entities;
+
+import dev.spimy.qoltweaks.config.RemovableConfigPaths;
+import dev.spimy.qoltweaks.modules.Module;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+
+public class AntiEndermanGrief extends Module {
+
+    public AntiEndermanGrief() {
+        super(
+            new RemovableConfigPaths[] {
+                RemovableConfigPaths.REQUIRE_PERMISSION
+            }
+        );
+    }
+
+    @EventHandler
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        if (event.getEntity().getType() != EntityType.ENDERMAN) return;
+        if (isDisabled()) return;
+        event.setCancelled(true);
+    }
+
+}
