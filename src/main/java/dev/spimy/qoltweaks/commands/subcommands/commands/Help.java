cf8ba103aa59db5f46cb159bdcba2dc1a84694a2
Spimy <officialspyme@gmail.com>
2023-01-30 23:45:09 +0400

    Refactor to use ArgumentInfo record class

--- src/main/java/dev/spimy/qoltweaks/commands/subcommands/commands/Help.java
+++ src/main/java/dev/spimy/qoltweaks/commands/subcommands/commands/Help.java
@@ -1,47 +1,50 @@
 package dev.spimy.qoltweaks.commands.subcommands.commands;
 
 import dev.spimy.qoltweaks.QoLTweaks;
+import dev.spimy.qoltweaks.commands.subcommands.ArgumentInfo;
 import dev.spimy.qoltweaks.commands.subcommands.SubCommand;
 import dev.spimy.qoltweaks.commands.subcommands.SubCommandHandler;
 import org.bukkit.command.CommandSender;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
+
+@SuppressWarnings("unused")
 public class Help extends SubCommand {
-
-    private final QoLTweaks plugin = QoLTweaks.getInstance();
 
     public Help() {
         super(
             "help",
             "View list of commands.",
             false,
-            new HashMap<>() {{
-                put("[command]", "View detailed help for the command provided.");
-            }}
+            new ArgumentInfo(
+                "command",
+                "View detailed help for the command provided.",
+                false
+                // List of arguments for this command specifically is set in onTabComplete method in BaseCommand
+            )
         );
     }
 
     @Override
     public boolean execute(CommandSender sender, String[] args) {
+        final QoLTweaks plugin = QoLTweaks.getInstance();
+        final SubCommandHandler handler = SubCommandHandler.getInstance();
+        final String commandName = plugin.getClass().getSimpleName().toLowerCase();
+
         if (args.length == 0) {
             sender.sendMessage(
                 plugin.getMessageManager().formatMessage(
                     String.format(
                         "&7-= &6QoLTweaks &7=-\n%s",
-                        String.join("\n", SubCommandHandler.getInstance().getCommandList())
+                        String.join("\n", getCommandList(commandName, handler))
                     )
                 )
             );
             return true;
         }
 
-        String command = args[0].toLowerCase();
-        SubCommandHandler handler = SubCommandHandler.getInstance();
-        SubCommand subCommand = handler.getSubCommand(command);
+        final SubCommand subCommand = handler.getSubCommand(args[0].toLowerCase());
 
         if (subCommand == null) {
             sender.sendMessage(plugin.getMessageManager().getConfigMessage("not-exist", true));
@@ -52,33 +55,48 @@ public class Help extends SubCommand {
             plugin.getMessageManager().formatMessage(
                 String.join("\n", new String[]
                     {
-                        String.format("&7-= &6Viewing detailed help for '%s' &7=-", command),
-                        String.format(
-                            "&6/%s %s %s: &a%s\n%s",
-                            plugin.getClass().getSimpleName().toLowerCase(),
-                            command,
-                            String.join(" ", subCommand.getArguments().keySet()),
-                            subCommand.getDescription(),
-                            String.join(
-                                "\n",
-                                formatArguments(subCommand.getArguments())
-                            )
-                        )
+                        String.format("&7-= &6Viewing detailed help for '%s' &7=-", subCommand.getName()),
+                        formatHelpInfo(commandName, subCommand, true),
                     }
                 )
             )
         );
+
         return true;
     }
 
-    private List<String> formatArguments(HashMap<String, String> arguments) {
-        final List<String> formattedArguments = new ArrayList<>();
+    private ArrayList<String> getCommandList(String commandName, SubCommandHandler handler) {
+        ArrayList<String> commandList = new ArrayList<>();
+        handler.getSubCommands().forEach(subCommand -> commandList.add(formatHelpInfo(commandName, subCommand, false)));
+        return commandList;
+    }
 
-        for (Map.Entry<String, String> entry : arguments.entrySet()) {
-            formattedArguments.add(String.format("&6%s: &a%s", entry.getKey(), entry.getValue()));
+    private String formatHelpInfo(String commandName, SubCommand subCommand, boolean detailed) {
+        if (subCommand.hasArguments()) {
+            final String helpInfo = String.format(
+                "&6/%s %s %s: &a%s",
+                commandName,
+                subCommand.getName(),
+                subCommand.getArgumentInfo().infoArg(),
+                subCommand.getDescription()
+            );
+            return (
+                detailed ? String.format(
+                    "%s\n&6%s: &a%s - %s",
+                    helpInfo,
+                    subCommand.getArgumentInfo().infoArg(),
+                    subCommand.getArgumentInfo().argDescription(),
+                    subCommand.getArgumentInfo().required() ? "REQUIRED" : "OPTIONAL"
+                ) : helpInfo
+            );
+        } else {
+            return String.format(
+                "&6/%s %s: &a%s",
+                commandName,
+                subCommand.getName(),
+                subCommand.getDescription()
+            );
         }
-
-        return formattedArguments;
     }
 
 }
--- src/main/java/dev/spimy/qoltweaks/commands/subcommands/commands/Toggle.java
+++ src/main/java/dev/spimy/qoltweaks/commands/subcommands/commands/Toggle.java
@@ -1,46 +1,49 @@
 package dev.spimy.qoltweaks.commands.subcommands.commands;
 
 import dev.spimy.qoltweaks.QoLTweaks;
+import dev.spimy.qoltweaks.commands.subcommands.ArgumentInfo;
 import dev.spimy.qoltweaks.commands.subcommands.SubCommand;
 import dev.spimy.qoltweaks.config.ConfigManager;
 import org.bukkit.command.CommandSender;
 
-import java.util.HashMap;
-
+@SuppressWarnings("unused")
 public class Toggle extends SubCommand {
-
-    private final QoLTweaks plugin = QoLTweaks.getInstance();
 
     public Toggle() {
         super(
             "toggle",
             "Enable or disable a module.",
             true,
-            new HashMap<>() {{
-                put("<module>", "Module to enable or disable.");
-            }}
+            new ArgumentInfo(
+                "module",
+                "Module to enable or disable.",
+                true,
+                QoLTweaks.getInstance().getConfigManagers().keySet().stream().toList()
+            )
         );
     }
     @Override
     public boolean execute(CommandSender sender, String[] args) {
+        final QoLTweaks plugin = QoLTweaks.getInstance();
+
         if (args.length == 0) {
             sender.sendMessage(plugin.getMessageManager().getConfigMessage("missing-argument", true));
             return true;
         }
 
-        String moduleName = args[0].toLowerCase();
-        ConfigManager configManager = plugin.getConfigManagers().get(moduleName);
+        final String moduleName = args[0].toLowerCase();
+        final ConfigManager configManager = plugin.getConfigManagers().get(moduleName);
 
         if (configManager == null) {
             sender.sendMessage(plugin.getMessageManager().getConfigMessage("invalid-argument", true));
             return true;
         }
 
-        boolean toggle = !configManager.getConfig().getBoolean("enabled");
+        final boolean toggle = !configManager.getConfig().getBoolean("enabled");
         configManager.getConfig().set("enabled", toggle);
         configManager.saveConfig();
 
-        String messagePath = toggle ? "module.on-enable" : "module.on-disable";
+        final String messagePath = toggle ? "module.on-enable" : "module.on-disable";
         sender.sendMessage(
             plugin.getMessageManager().getConfigMessage(messagePath, true)
                 .replaceAll("\\{\\bmodule\\b}", moduleName)
