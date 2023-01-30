35e21a71a82135ae07235d22229dd91068051b08
Spimy <officialspyme@gmail.com>
2023-01-31 00:17:20 +0400

    Refactor langConfig to be a global variable

--- src/main/java/dev/spimy/qoltweaks/MessageManager.java
+++ src/main/java/dev/spimy/qoltweaks/MessageManager.java
@@ -5,20 +5,18 @@ import org.bukkit.configuration.ConfigurationSection;
 
 public class MessageManager {
 
-    private final QoLTweaks plugin = QoLTweaks.getInstance();
+    private final ConfigurationSection langConfig = QoLTweaks.getInstance().getConfig().getConfigurationSection("lang");
 
     public String formatMessage(final String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 
     public String prefixedMessage(String message) {
-        final ConfigurationSection langConfig = plugin.getConfig().getConfigurationSection("lang");
         message = langConfig != null ? langConfig.getString("prefix") + " " + message : message;
         return formatMessage(message);
     }
 
     public String getConfigMessage(final String path, final boolean prefixed) {
-        final ConfigurationSection langConfig = plugin.getConfig().getConfigurationSection("lang");
         final String message = langConfig != null ? langConfig.getString(path) : "language config was not found";
         return prefixed ? prefixedMessage(message) : formatMessage(message);
     }
