b76f8f76dec814168cb2644d0cc83a6c6c007b7c
Spimy <officialspyme@gmail.com>
2023-01-30 23:42:11 +0400

    Remove getCommandList method from SubCommandHandler

--- src/main/java/dev/spimy/qoltweaks/commands/subcommands/SubCommandHandler.java
+++ src/main/java/dev/spimy/qoltweaks/commands/subcommands/SubCommandHandler.java
@@ -1,6 +1,5 @@
 package dev.spimy.qoltweaks.commands.subcommands;
 
-import dev.spimy.qoltweaks.QoLTweaks;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
@@ -37,22 +36,12 @@ public class SubCommandHandler {
         return subCommands.stream().filter(subCommand -> subCommand.getName().equalsIgnoreCase(command)).findFirst().orElse(null);
     }
 
-    public List<String> getSubCommandNames() {
-        return this.subCommands.stream().map(SubCommand::getName).collect(Collectors.toList());
+    public List<SubCommand> getSubCommands() {
+        return subCommands;
     }
 
-    public ArrayList<String> getCommandList() {
-        ArrayList<String> commandList = new ArrayList<>();
-
-        subCommands.forEach(subCommand -> commandList.add(String.format(
-            "&6/%s %s %s: &a%s",
-            QoLTweaks.getInstance().getClass().getSimpleName().toLowerCase(),
-            subCommand.getName(),
-            String.join(" ", subCommand.getArguments().keySet()),
-            subCommand.getDescription()
-        )));
-
-        return commandList;
+    public List<String> getSubCommandNames() {
+        return subCommands.stream().map(SubCommand::getName).collect(Collectors.toList());
     }
 
 }
